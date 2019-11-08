94f8d3982f6c76d880a928189c92f584d0db6e6c
Victoria Hale <vic.hale74@gmail.com>
2019-11-08 03:10:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,8 +51,8 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
   
-  def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = '#{attribute}'"
+  def self.find_by(attr_hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attr_hash.keys[0].to_s} = '#{attr_hash.values[0].to_s}'"
     DB[:conn].execute(sql)
     
   end
