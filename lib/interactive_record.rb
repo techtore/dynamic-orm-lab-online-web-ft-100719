8d1c0c08eb4931c504af759a4977d33951e61729
Victoria Hale <vic.hale74@gmail.com>
2019-11-08 02:43:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,7 +7,7 @@ class InteractiveRecord
   end
   
   def self.column_names
-    sql = "pragma table_info ('#{table_names}')"
+    sql = "pragma table_info ('#{table_name}')"
     
     table_info = DB[:conn].execute(sql)
     column_names = []
