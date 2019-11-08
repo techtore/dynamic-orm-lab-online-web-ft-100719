9981a6edbdb5867da21b0f602471ee881b007919
Victoria Hale <vic.hale74@gmail.com>
2019-11-08 02:39:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,17 +7,6 @@ class InteractiveRecord
   end
   
   def self.column_names
-    DB[:conn].results_as_a_hash = true 
-    sql = "pragma table_info ('#{table_name}')" #queries table for name of columns
     
-    table_info =  DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |col|
-      column_names << col["name"]
-    end
-    column_names.compact
   end
-    self.column_names.each do |col_name|
-      attr_accessor col_name.to_sym
-    end
 end
